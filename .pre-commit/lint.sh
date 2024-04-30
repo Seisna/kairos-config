854737f3ae28d7fe5ce13220ad01e43f429578c9
Seisna <99881694+Seisna@users.noreply.github.com>
2024-04-30 14:10:47 +0200

    Add files via upload

---
+++ .pre-commit/lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+version=v2.3.1
+
+set -e
+
+command=docker
+
+if command nerdctl &>/dev/null; then
+  command=nerdctl
+fi
+
+$command run --rm -v $PWD:/configs ghcr.io/tyzbit/kairosctl:$version sh -c \
+"""
+find . -name cloud-config.yaml | while read config; do 
+  echo -n \"\$config: \"; 
+  kairosctl validate \$config;
+  if [[ \$? -eq 0 ]]; then 
+    echo \"valid\";
+  else
+    exit 1
+  fi
+done
+""" >&1
